d146c32c8acf2ad3ea90d59961355b9d6a428866
rgarro <rgarro@gmail.com>
2023-06-19 04:50:19 -0500

    acme brand ...

--- apps/sqlitetest/config.php
+++ apps/sqlitetest/config.php
@@ -1,2 +1,16 @@
 <?php
+/**
+ * __________________|      |____________________________________________
+ *     ,--.    ,--.          ,--.   ,--.
+ *    |oo  | _  \  `.       | oo | |  oo|
+ *o  o|~~  |(_) /   ;       | ~~ | |  ~~|o  o  o  o  o  o  o  o  o  o  o
+ *    |/\/\|   '._,'        |/\/\| |/\/\|
+ *__________________        ____________________________________________
+ *                  |      |dwb
+ *
+ * Will make a red sabre or maze bulls eyes on boby trap\ativation
+ * 
+ * @author Rolando<rolando@emptyart.xyz>
+ */
 $dataBasePath = "db/chinook.db";
+$path = getcwd();
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -26,23 +26,19 @@ use PHPUnit\Framework\TestCase;
  *   __)/ / \
  *--"--(_.Ooo'----
  *
- * La Teta De Mondongui, visiones de los tiemps de miguel angel rodriguez
+ * La Teta De Mondongui, visiones de los tiempos de miguel angel rodriguez
  * Michael Maher desaparecio, Avantica desaparecio, muertos de hambre de sykes
  * En un mitin de la Anep retaguardia en la clinica marcial fallas
  * gas zarin sin antidoto que nos dio kadafi
+ * mataron una secretaria de essential costarica en el volcan poas
+ * dictadura gay de fabricio
+ * 
+ * @author Rolando<rolando@emptyart.xyz>
  */
 final class MainTest extends TestCase
 {
     public function testWeCanTest(): void
     {
-        $stack = [];
-        $this->assertSame(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        $this->assertTrue(true);
     }
 }
