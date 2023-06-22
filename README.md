b6a9c8bd55b0be1b03bd183ba91358b540a69417
rgarro <rgarro@gmail.com>
2023-06-22 13:57:20 -0500

    initializing tdd

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # cookieMonster2
 
-A RestFul Basecamp to assert crossing swift creek and keep depositing paychecks on Capital One.
-a storm made a tree fell at swift creek bridge 2 days ago
+A RestFul Basecamp to assert crossing swift creek every shift and keep depositing paychecks on Capital One.
 they will send me a slingshot mixer kite surfboard from hood river ..
 
 [![aqui cayo la paloma en la laguna de nuzco ... ](https://raw.githubusercontent.com/rgarro/cookieMonster2/master/cookie2.png)]
---
+++ tests/sqlitetest/MainTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+/**
+ * Transitioning the 14knots winds
+ *         -.--.
+ *         )  " '-,
+ *         ',' 2  \_
+ *          \q \ .  \
+ *       _.--'  '----.__
+ *      /  ._      _.__ \__
+ *   _.'_.'  \_ .-._\_ '-, }
+ *  (,/ _.---;-(  . \ \   ~
+ *____ (  .___\_\  \/_/
+ *(      '-._ \   \ |
+ * '._       ),> _) >
+ *   '-._ c='  Cooo  -._
+ *       '-._           '.
+ *           '-._         `\
+ *      snd      '-._       '.
+ *                   '-._     \
+ *                       `~---'
+ *
+ * USPS Visions the darmakaya of ridding a slingshot mixer kiteboard downwinding on a sunny evening ...
+ * Test the main model of a future tree of resful responders for swift apps on my iWatch or iPhone
+ * anticipate not to lose you twintip after jumps buy a secon hand snowboard and assert true your down hill on snowbird some day
+ * alabama spirits leaving magic weed in my kitesurf spot
+ * 
+ * @author Rolando<rolando@emptyart.xyz>
+ */
+final class MainTest extends TestCase
+{
+    public function testWeCanTest(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDbPdoIsOK():void{ 
+        $this->assertTrue(true); 
+    }
+}
