0499d6a9adacc682d04028e86a65063f46251f77
rgarro <rgarro@gmail.com>
2023-06-27 12:58:02 -0500

    start your day with black rifle ..

--- lib/emptyLib/MVC/Model/PlumaDeAguilaSqlite.php
+++ lib/emptyLib/MVC/Model/PlumaDeAguilaSqlite.php
@@ -8,14 +8,19 @@
  *     \___________  ___________/
  *               /____\
  * 
- * Yo No soy miembro del partido nazi malparidos
  * Yo soy un Comanche !! Yo soy un Comanche 
- * cara palida mato al delfin de mysql en el tipi de pluma punto
- * oracle es el guano de las estrellas que estan en la cola de procesos
+ * Vendiamos rifles negros en la afueras de Fuerte Walton
+ * 
+ * 
  * 
  * 
  * @author Rolando <rolando@emptyart.xyz>
  */
 abstract class PlumaDeAguilaSqlite {
 
+    protected $tableName = "cajaDeRifles";
+    
+    function __construct() {
+
+    }
 }
