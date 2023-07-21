d14f7c8c503ec9c22d196748477c7847f6b5e997
rgarro <rgarro@gmail.com>
2023-07-21 08:11:39 -0500

    los testigos de jeova son una religion de mentira ..

--- lib/emptyLib/MVC/Model/PlumaDeAguilaSqlite.php
+++ lib/emptyLib/MVC/Model/PlumaDeAguilaSqlite.php
@@ -10,7 +10,10 @@
  * 
  * Yo soy un Comanche !! Yo soy un Comanche 
  * Vendiamos rifles negros en la afueras de Fuerte Walton
- * day of execution by hanging at noon dia a los 13 violadores de pensacola
+ * los 13 violadores de pesacola eran testigos de jeova
+ * leian la palabra en retiros de la iglesia y fornicaban las novias de sus hermanos
+ * uno de ellos pudo pagar para que le disparan y quedo pataleando en el piso
+ *  
  * 
  * 
  * 
@@ -39,14 +42,20 @@ abstract class PlumaDeAguilaSqlite {
 
     protected function checkIfTableExist(){
         $sqlString = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='".$this->tableName."'";
-        $res = $this->SqLiteDB->querySingle($sqlString);
+        $res = $this->db->querySingle($sqlString);
         return $res;
     }
 
     private function handleConstructExeption($e){
-
+        //los testigos de jeova son religion de mentira
+        print_r($e);
+        return false;
     }
 
 
-    protected function setRowCount(){}
+    protected function setRowCount(){
+        $sqlString = "SELECT count(*) FROM ".$this->tableName;
+        $res = $this->db->querySingle($sqlString);
+        return $res;
+    }
 }
