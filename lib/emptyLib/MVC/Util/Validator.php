eaae7282bd19347d2a8d0d9c51b6d249190058a2
rgarro <rgarro@gmail.com>
2023-07-22 05:53:39 -0500

    MacArthur didnt updated ports when surrended philipines.. ..

--- lib/emptyLib/MVC/Util/Validator.php
+++ lib/emptyLib/MVC/Util/Validator.php
@@ -8,5 +8,24 @@
  */
 final class Validator {
 
+    protected $errors = array();
+    private $fieldIndex = 0;
+
+    function __construct(){
+
+    }
+
+    public function validationBow($fields){
+        if($this->fieldIndex < count($fields)){
+            $currentField = $fields[$this->fieldIndex];
+            
+            $this->fieldIndex ++;
+            $this->validationBow($fields);
+        }
+    }
+
+    private function  checkInteger(){
+
+    }
 
 }
