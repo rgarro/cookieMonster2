12688bb3d4f9896c23c743668c769b4d7955f418
rgarro <rgarro@gmail.com>
2023-07-23 00:08:14 -0500

    sun is shinning the weather is sweet ....

--- lib/kitesurfsess/MVC/Model/Objects/Kite.php
+++ lib/kitesurfsess/MVC/Model/Objects/Kite.php
@@ -6,6 +6,7 @@
  * @author Rolando<rolando@emptyart.xyz>
  */
 include_once __DIR__."/../../../../emptyLib/MVC/Model/TomaHawkPocoObject.php";
+include_once __DIR__."/../../../../emptyLib/MVC/Util/Validator.php";
 
 class Kite extends TomaHawkPocoObject{
 
@@ -13,7 +14,11 @@ class Kite extends TomaHawkPocoObject{
     protected $name;
     protected $area;
 
-    public $fields = array("id"=>0,"name"=>"","area"=>0.0);
+    public $fields = array(
+                        "id"=>array('value'=>0,"validator"=>"int"),
+                        "name"=>array("value"=>""),
+                        "area"=>array("value"=>0.0)
+                    );
 
     function __construct(){
         parent::__construct();
