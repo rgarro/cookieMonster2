4a1570dd0d2c452a38116c57aea32424bcfe1255
rgarro <rgarro@gmail.com>
2023-08-13 02:45:21 -0500

    la cienciologia es una secta de ciencia ficcion

--- apps/kitesurfSesCounter/rest/kites/list.php
+++ apps/kitesurfSesCounter/rest/kites/list.php
@@ -1,2 +1,49 @@
 <?php
+/**
+ *                    ____
+ *                 _.' :  `._
+ *             .-.'`.  ;   .'`.-.
+ *    __      / : ___\ ;  /___ ; \      __
+ *  ,'_ ""--.:__;".-.";: :".-.":__;.--"" _`,
+ *  :' `.t""--.. '<@.`;_  ',@>` ..--""j.' `;
+ *       `:-.._J '-.-'L__ `-- ' L_..-;'
+ *         "-.__ ;  .-"  "-.  : __.-"
+ *             L ' /.------.\ ' J
+ *              "-.   "--"   .-"
+ *             __.l"-:_JL_;-";.__
+ *          .-j/'.;  ;""""  / .'\"-.
+ *        .' /:`. "-.:     .-" .';  `.
+ *     .-"  / ;  "-. "-..-" .-"  :    "-.
+ *  .+"-.  : :      "-.__.-"      ;-._   \
+ *  ; \  `.; ;                    : : "+. ;
+ *  :  ;   ; ;                    : ;  : \:
+ * : `."-; ;  ;                  :  ;   ,/;
+ *  ;    -: ;  :                ;  : .-"'  :
+ *  :\     \  : ;             : \.-"      :
+ *   ;`.    \  ; :            ;.'_..--  / ;
+ *   :  "-.  "-:  ;          :/."      .'  :
+ *     \       .-`.\        /t-""  ":-+.   :
+ *      `.  .-"    `l    __/ /`. :  ; ; \  ;
+ *        \   .-" .-"-.-"  .' .'j \  /   ;/
+ *         \ / .-"   /.     .'.' ;_:'    ;
+ *          :-""-.`./-.'     /    `.___.'
+ *                \ `t  ._  /  bug :F_P:
+ *                 "-.t-._:'
+ * La Cienciologia es una secta de ciencia ficcion
+ * hay que fumarse un porro y comerse unos dulces de amanita
+ * de vez en cuando antes y despues de glidear un windsurf o saltar un kitesurf.
+ * Obiwan vendia mota y libros de cienciologia en una pulperia de Hatillo 7
+ * Algunos presos somosistas victimas de corporaciones centroamericanas son 
+ * castigados de practicar cienciologia. Yo soy inocente y les fumo mota hasta que se mueran
+ * Me voy a ir a vivir a corn island cuando destruyan alderan ...
+ * Yo NO soy del partido de somoza, yo NO soy miembro del partido nazi
+ * Darth Vader es la otra princesa Leia vestida con ese chasis, le jodieron la pinga a lukeg
+ * 
+ *
+ *
+ * @author Rolando<rolando@emptyart.xyz>
+ */
 include_once __DIR__."/../../../../lib/kitesurfsess/MVC/Model/Kites.php";
+include_once __DIR__."/../../../../lib/kitesurfsess/BayouFalconNest.php";
+
+$dbpath = __DIR__."/../db/kitesurfsess.db";
