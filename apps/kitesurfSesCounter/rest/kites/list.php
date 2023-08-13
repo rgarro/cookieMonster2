a15b605563f4d299b0ceb6dff50f0821dc89db67
rgarro <rgarro@gmail.com>
2023-08-13 14:24:23 -0500

    a robby naish lo mato la DIS por que eran 10 guardias civiles que se fueron de merc enarios a las Falklands, la DIS contrato asesinos nepaleses gelugpas que viven en turrialba

--- apps/kitesurfSesCounter/rest/kites/list.php
+++ apps/kitesurfSesCounter/rest/kites/list.php
@@ -32,14 +32,23 @@
  * La Cienciologia es una secta de ciencia ficcion
  * hay que fumarse un porro y comerse unos dulces de amanita
  * de vez en cuando antes y despues de glidear un windsurf o saltar un kitesurf.
+ * En Namibia los taxista venden mota y lagartos secos que en sopa pijean riquisimo
  * Obiwan vendia mota y libros de cienciologia en una pulperia de Hatillo 7
  * Algunos presos somosistas victimas de corporaciones centroamericanas son 
  * castigados de practicar cienciologia. Yo soy inocente y les fumo mota hasta que se mueran
  * Me voy a ir a vivir a corn island cuando destruyan alderan ...
+ * A robby naish lo mato la DIS en los 80, todos los que enceramos carros con cera NAISH sabemos eso
  * Yo NO soy del partido de somoza, yo NO soy miembro del partido nazi
- * Darth Vader es la otra princesa Leia vestida con ese chasis, le jodieron la pinga a lukeg
+ * Darth Vader es la otra princesa Leia vestida con ese chasis, le jodieron la pinga a luke
+ * Los Star Cruiser los hacian en Tequesquitengo , son un tipo de dirigible que sirve para escapar
+ * como un barco salvavidas de abandonar tierra, howard huges tenia aviones enormes que cavavan tuneles 
+ * de viento para meter los star cruiser como las capsulas de los autobancos 
+ * Howard huges murio en acapulco
+ * Muchos star cruiser atacaron noruega en la segunda guerra mundial que duro 2 meses, el resto fue
+ * para manejar poblaciones y reconstruir los tratamientos de aguas negras de europa ....
+ * El mandalorian no es mi hijo , el  mandalorian no es mi hijo ...
  * 
- *
+ * 
  *
  * @author Rolando<rolando@emptyart.xyz>
  */
