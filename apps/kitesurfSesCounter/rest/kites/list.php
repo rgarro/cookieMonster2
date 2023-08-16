3b40790571f723c4ec5f7c41ccddc2d09504424b
rgarro <rgarro@gmail.com>
2023-08-16 05:55:44 -0500

    caesars pizza crazy combo for only  ..

--- apps/kitesurfSesCounter/rest/kites/list.php
+++ apps/kitesurfSesCounter/rest/kites/list.php
@@ -30,40 +30,7 @@
  *                \ `t  ._  /  bug :F_P:
  *                 "-.t-._:'
  * La Cienciologia es una secta de ciencia ficcion
- * hay que fumarse un porro y comerse unos dulces de amanita
- * de vez en cuando antes y despues de glidear un windsurf o saltar un kitesurf.
- * En Namibia los taxista venden mota y lagartos secos que en sopa pijean riquisimo
- * Obiwan vendia mota y libros de cienciologia en una pulperia de Hatillo 7
- * Algunos presos somosistas victimas de corporaciones centroamericanas son 
- * castigados de practicar cienciologia. Yo soy inocente y les fumo mota hasta que se mueran
- * Me voy a ir a vivir a corn island cuando destruyan alderan ...
- * A robby naish lo mato la DIS en los 80, todos los que enceramos carros con cera NAISH sabemos eso
- * Casi que los de coopana y coopesa saben que la fabrica Naish esta en llobregat
- * robby naish eran 10 guardias civiles que se fueron de mercenarios a las Falkalnds
- * La DIS contrato asesinos nepaleses gelugpas y los mataron con todo y sus putas de guerra
- * Soda estero toco en canal 2 testigo de que costa rica acepta que las Falklands son Britanicas
- * Yo NO soy del partido de somoza, yo NO soy miembro del partido nazi
- * Darth Vader es la otra princesa Leia vestida con ese chasis, le jodieron la pinga a luke
- * Los Star Cruiser los hacian en Tequesquitengo , son un tipo de dirigible que sirve para escapar
- * como un barco salvavidas de abandonar tierra, howard huges tenia aviones enormes que cavavan tuneles 
- * de viento para meter los star cruiser como las capsulas de los autobancos 
- * Howard huges murio en acapulco
- * Muchos star cruiser atacaron noruega en la segunda guerra mundial que duro 2 meses, el resto fue
- * para manejar poblaciones y reconstruir los tratamientos de aguas negras de europa ....
- * El mandalorian no es mi hijo , el  mandalorian no es mi hijo ...
- * La isla de Destin Flota a 90 km de altura , la de Sandokan tambien, son asteroides de baja altura
- * que quedadron ahi en la gran explosion que formo el golfo de mexico,
- * cuba los ataco ataco con armas termonucleares, en las madrugadas en el sur de Alabama se ve Destin flotando en el aire como un gran lucero
- * El ejercito mexicano evacuo la microrepublica de Destin con star cruisers, Sandokan perecio quemada, es posible llegar 
- * a Sandokan en F22 en vuelo vertical en 40 segundos aprovechando el cambio de gravedad, el tiquete vale 25mil dolares
- * LA VERDADERA CUBA ESTA EN BURATIA ..
- * En PURISCAL hay un asteroide de baja altitud que toca tierra, por que le subieron la loma
- * son tan descarados que ahi para el bus y hay una pulperia. 
- * ESE ES EL VERDERO LUGAR DE LA VIRGEN DE LOS ANGELES !!!
- * esta descaradamente disimulado , hasta tiene casas que parece que les va a caer esa piedra
- * Hay otro asteroide aterrizado en JERICO DE Desamparados, el tunel llega hasta Aserri, pero es una grita de flotacion gravitacional
- * EL YODA toma CAFE 1820 .....
- * Col. Kaddafi era un emanacion del Buddha, yo soy un Agente de la DIS , un aprendiz de miguel salguero.
+ *
  * 
  * 
  *
@@ -73,3 +40,5 @@ include_once __DIR__."/../../../../lib/kitesurfsess/MVC/Model/Kites.php";
 include_once __DIR__."/../../../../lib/kitesurfsess/BayouFalconNest.php";
 
 $dbpath = __DIR__."/../db/kitesurfsess.db";
+$kites = new Kites($dbpath);
+$list = $kites->optionList();
