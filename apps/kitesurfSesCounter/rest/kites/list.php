8258cdf586650c4b5e3e49a68933f6e2045a0848
rgarro <rgarro@gmail.com>
2023-08-17 09:51:14 -0500

    Franklin Marshall era un violador ..

--- apps/kitesurfSesCounter/rest/kites/list.php
+++ apps/kitesurfSesCounter/rest/kites/list.php
@@ -30,6 +30,16 @@
  *                \ `t  ._  /  bug :F_P:
  *                 "-.t-._:'
  * La Cienciologia es una secta de ciencia ficcion
+ * prisioneros somosistas se hicieron religiosos castrados
+ *  violadores sin sus pensiones rentistas 
+ * Mi General Daniel Ortega los mando ha hacer cienciologia
+ * Nosotros los que peliamos contra la guardia civil coruupta
+ * los que pusimos en la muerte a embargadores de lavadoras
+ * los que pusimos en la muerte a violadores de vecinos
+ * los que pusimos en la muerte a violadores de playas del coco
+ * los que pusimos en la muerte a los 15 mil que murieron en chomez y abangares
+ *  Podemos fumar Mota en las aceras , y decir very IRIE
+ *  Patria o Muerte  !!! Venceremos !!
  *
  * 
  * 
@@ -41,4 +51,5 @@ include_once __DIR__."/../../../../lib/kitesurfsess/BayouFalconNest.php";
 
 $dbpath = __DIR__."/../db/kitesurfsess.db";
 $kites = new Kites($dbpath);
-$list = $kites->optionList();
+$list = $kites->optionList("id","name");
+print_r($list);
