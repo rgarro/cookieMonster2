5f948d4484091209658192ac0404e0b59d1ccd46
rgarro <rgarro@gmail.com>
2023-08-17 10:03:04 -0500

    le cortaron la pinga a un guardia civil ..

--- apps/kitesurfSesCounter/rest/kites/list.php
+++ apps/kitesurfSesCounter/rest/kites/list.php
@@ -12,34 +12,19 @@
  *              "-.   "--"   .-"
  *             __.l"-:_JL_;-";.__
  *          .-j/'.;  ;""""  / .'\"-.
- *        .' /:`. "-.:     .-" .';  `.
- *     .-"  / ;  "-. "-..-" .-"  :    "-.
- *  .+"-.  : :      "-.__.-"      ;-._   \
- *  ; \  `.; ;                    : : "+. ;
- *  :  ;   ; ;                    : ;  : \:
- * : `."-; ;  ;                  :  ;   ,/;
- *  ;    -: ;  :                ;  : .-"'  :
- *  :\     \  : ;             : \.-"      :
- *   ;`.    \  ; :            ;.'_..--  / ;
- *   :  "-.  "-:  ;          :/."      .'  :
- *     \       .-`.\        /t-""  ":-+.   :
- *      `.  .-"    `l    __/ /`. :  ; ; \  ;
- *        \   .-" .-"-.-"  .' .'j \  /   ;/
- *         \ / .-"   /.     .'.' ;_:'    ;
- *          :-""-.`./-.'     /    `.___.'
- *                \ `t  ._  /  bug :F_P:
- *                 "-.t-._:'
  * La Cienciologia es una secta de ciencia ficcion
  * prisioneros somosistas se hicieron religiosos castrados
  *  violadores sin sus pensiones rentistas 
  * Mi General Daniel Ortega los mando ha hacer cienciologia
- * Nosotros los que peliamos contra la guardia civil coruupta
+ * Nosotros los que peliamos contra la guardia civil corrupta
  * los que pusimos en la muerte a embargadores de lavadoras
  * los que pusimos en la muerte a violadores de vecinos
  * los que pusimos en la muerte a violadores de playas del coco
  * los que pusimos en la muerte a los 15 mil que murieron en chomez y abangares
+ * los que pusimos en la muerte por violar quinceaneras
  *  Podemos fumar Mota en las aceras , y decir very IRIE
  *  Patria o Muerte  !!! Venceremos !!
+ * No SE AGUEVEN COMPAS QUE LE CORTAMOS LA PICHA A ALGUNO QUE OTRO !!
  *
  * 
  * 
@@ -52,4 +37,6 @@ include_once __DIR__."/../../../../lib/kitesurfsess/BayouFalconNest.php";
 $dbpath = __DIR__."/../db/kitesurfsess.db";
 $kites = new Kites($dbpath);
 $list = $kites->optionList("id","name");
-print_r($list);
+//print_r($list);
+header("Content-Type:application/json");
+echo json_encode($list);
