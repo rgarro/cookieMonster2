e6d2ec1fd81308e7bdebcf56986d2bc8384a1869
rgarro <rgarro@gmail.com>
2023-09-15 00:22:11 -0500

    aloute je te plumaaire

--- lib/kitesurfsess/MVC/Model/Kites.php
+++ lib/kitesurfsess/MVC/Model/Kites.php
@@ -4,6 +4,10 @@
  * suddenly whiped out in the downwind glide, his guru was floating the zenith and suddenly yelled
  * to triplicate the math procesor of a silicon graphics you need to cool down the room 
  * until -7celsius degrees, then instructed him on why not to use SolidWorks or heretic Autodesk
+ * Master, replied Kanhapa , hot air welders are from beijing, liu bei betrayed the fifth dalai lama
+ * Nobody buys crabs from the deepens of baikal lake anymore, the mig 21 engine ionizes levitating gases
+ * the bonpos from planet nimbo will trade them for new harware
+ * 
  * 
  * 
  * @author Rolando<rolando@emptyart.xyz>
@@ -14,7 +18,11 @@ include_once __DIR__."/Objects/Kite.php";
 class Kites extends PlumaDeAguilaSqlite {
 
     protected $tableName = "kites";
-    public $validator = array("id"=>array("int"),"name"=>array("str"=>array("required"=>1,"maxlenght"=>50)));
+    public $validator = array(
+                            "id"=>array("int"=>"int"),
+                            "name"=>array("str"=>"str","required"=>1,"maxlenght"=>50),
+                            "area"=>array("int"=>"int","required"=>1,"min"=>2,"max"=>22),
+                        );
 
     function __construct($sqLiteDB){       
         parent::__construct($sqLiteDB);
--- lib/kitesurfsess/MVC/Model/Users.php
+++ lib/kitesurfsess/MVC/Model/Users.php
@@ -1,6 +1,20 @@
 <?php
 /**
+ *                .---.
+ *           '-.  |   |  .-'         
+ *             ___|   |___          
+ *        -=  [           ]  =-    
+ *            `---.   .---'         
+ *         __||__ |   | __||__      
+ *         '-..-' |   | '-..-'   
+ *           ||   |   |   ||     
+ *           ||_.-|   |-,_||     
+ *         .-"`   `"`'`   `"-.   
+ *jgs    .'                   '.
+ * LOS JOTOS NO TIENEN CURA o EL RETO DE JOTO
  * Cast: Jimmy Wang Yu, Lo Lieh, Lung Fei, Chung Hwa, Chen Hung Lieh
+ * Tecnicolor
+ * 
  */
 include_once __DIR__."/../../../emptyLib/MVC/Model/PlumaDeAguilaSqlite.php";
 include_once __DIR__."/Objects/Kite.php";
